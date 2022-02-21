fde641b20204d2eaf0217ad1e6657ada525b5016
sdxsd <willmaguire@icloud.com>
2022-02-21 12:27:29 +0100

    Mister Bones's Wild Ride does not end

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ A program is free software if users have all of these freedoms.
 #include "../include/so_long.h"
 #include "../include/parse.h"
 
-typedef struct	s_data {
+typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -37,22 +37,46 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-static void belgie(void *mlx, int blksiz)
+static void	belgie(void *mlx, int x, int y, int blksiz)
 {
 	t_data	img;
+	t_data	img2;
 	void	*win;
-	win = mlx_new_window(mlx, 128, 128, "so_long");
-	img.img = mlx_xpm_file_to_image(mlx, "./images/wall.xpm", &blksiz, &blksiz);
+	int		imgx;
+	int		imgy;
+	int		pixx;
+	int		pixy;
+	int		colour;
+
+	pixx = x * blksiz;
+	pixy = y * blksiz;
+	imgx = 32;
+	imgy = 32;
+	colour = 0xFFFFFFFF;
+	win = mlx_new_window(mlx, x * blksiz, y * blksiz, "so_long");
+	img.img = mlx_xpm_file_to_image(mlx, "./images/wall.xpm", &imgx, &imgy);
+	img2.img = mlx_xpm_file_to_image(mlx, "./images/player.xpm", &imgx, &imgy);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
+	img2.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx_put_image_to_window(mlx, win, img.img, x, y);
+	mlx_put_image_to_window(mlx, win, img.img, 32, 0);
+	mlx_put_image_to_window(mlx, win, img2.img, 0, 32);
+	for (int i = 100; i > 0; i--)
+	{
+		if (!mlx_pixel_put(mlx, win, pixx, pixy, colour))
+			return ;
+		pixx--;
+		pixy--;
+	}
 }
 
 int	main(int argc, char *argv[])
 {
 	t_the_matrix	*sim_env;
 	void			*mlx;
-	int	blksiz = 128;
+	int				blksiz;
 
+	blksiz = 32;
 	mlx = mlx_init();
 	sim_env = matrix_init(argc, argv);
 	if (!sim_env)
@@ -62,6 +86,6 @@ int	main(int argc, char *argv[])
 	}
 	ft_printf("X: %d\nY: %d\n", sim_env -> x, sim_env -> y);
 	ft_printf("BLKNUM: %d\n", sim_env -> x * sim_env -> y);
-	belgie(mlx, blksiz);
+	belgie(mlx, sim_env -> x, sim_env -> y, blksiz);
 	mlx_loop(mlx);
 }
